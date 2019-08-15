5e4ac17a0c12ccdb7d66e85ef3567fe711ea2cc7
xamuel98 <samuelakintomiwa98@gmail.com>
2019-08-15 22:39:38 +0100

    Update Settings

--- btre/settings.py
+++ btre/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['btreholmes.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1', 'btreholmes.herokuapp.com']
 
 
 # Application definition
